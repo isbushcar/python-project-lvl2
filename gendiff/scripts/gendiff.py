c13c7be1f60812cc3a86136976075ac71c7d6239
andrew <isbushcar@gmail.com>
2021-03-31 18:54:39 +1000

    test commit

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,14 @@
+#!usr/bin/env python3
+
+
+import argparse
+
+parser = argparse.ArgumentParser(prog='generate diff', description='Generate diff')
+parser.add_argument('first_file')
+parser.add_argument('second_file')
+
+args = parser.parse_args()
+
+
+def main():
+    pass
