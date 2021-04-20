8eb22462e51665fb73e517a85f108f5e55907e64
berezin <isbushcar@gmail.com>
2021-04-21 09:34:30 +1000

    plain.py  updated due to internal representation logic change

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -7,27 +7,26 @@ import json
 def plain(diff_tree, path=''):  # noqa: WPS210, WPS231, C901
     """Return plain diff from diff tree."""
     diff_output = ''
-    for key, keys_value in diff_tree.items():
-        current_key, status = key if isinstance(key, tuple) else (key, None)
-        current_key = f'{path}.{current_key}' if path else current_key
-        if diff_output and status not in {'unchanged', None}:
-            diff_output += '\n'  # adding a new line only if needed
+    for key, _ in diff_tree.items():
+        status = diff_tree[key].get('status')
+        keys_value = diff_tree[key]['value']
+        current_key = f'{path}.{key}' if path else key
         if status == 'nested':
-            diff_output += plain(keys_value, current_key)
+            diff_output += plain(keys_value, current_key) + '\n'
         elif status == 'changed':
-            current_value = format_value(keys_value[1])
-            old_value = format_value(keys_value[0])
+            current_value = get_formatted_value(keys_value['new value'])
+            old_value = get_formatted_value(keys_value['old value'])
             diff_output += f"Property '{current_key}' was updated. "
-            diff_output += f'From {old_value} to {current_value}'
+            diff_output += f'From {old_value} to {current_value}\n'
         elif status == 'deleted':
-            diff_output += f"Property '{current_key}' was removed"
+            diff_output += f"Property '{current_key}' was removed\n"
         elif status == 'added':
-            current_value = format_value(keys_value)
-            diff_output += f"Property '{current_key}' was added with value: {current_value}"  # noqa: E501
-    return diff_output
+            current_value = get_formatted_value(keys_value)
+            diff_output += f"Property '{current_key}' was added with value: {current_value}\n"  # noqa: E501
+    return diff_output.rstrip()
 
 
-def format_value(keys_value):
+def get_formatted_value(keys_value):
     """Return formatted value to add to output."""
     if isinstance(keys_value, dict):
         return '[complex value]'
