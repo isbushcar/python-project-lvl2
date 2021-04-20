ff9dca6f5f4e1581030d5c30803768a2924891a2
berezin <isbushcar@gmail.com>
2021-04-20 19:16:46 +1000

    refactored plain.py (updated logic of plain function)

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -8,26 +8,22 @@ def plain(diff_tree, path=''):  # noqa: WPS210, WPS231, C901
     """Return plain diff from diff tree."""
     diff_output = ''
     for key, keys_value in diff_tree.items():
-        if not isinstance(key, tuple) or key[1] == 'unchanged':
-            continue
-        if diff_output:
-            diff_output += '\n'
-        current_key, status = key
+        current_key, status = key if isinstance(key, tuple) else (key, None)
         current_key = f'{path}.{current_key}' if path else current_key
+        if diff_output and status not in {'unchanged', None}:
+            diff_output += '\n'
         if status == 'nested':
             diff_output += plain(keys_value, current_key)
-            continue
-        if status == 'changed':
+        elif status == 'changed':
             current_value = format_value(keys_value[1])
             old_value = format_value(keys_value[0])
             diff_output += f"Property '{current_key}' was updated. "
             diff_output += f'From {old_value} to {current_value}'
-            continue
-        current_value = format_value(keys_value)
-        if status == 'deleted':
+        elif status == 'deleted':
             diff_output += f"Property '{current_key}' was removed"
-            continue
-        diff_output += f"Property '{current_key}' was added with value: {current_value}"  # noqa: E501
+        elif status == 'added':
+            current_value = format_value(keys_value)
+            diff_output += f"Property '{current_key}' was added with value: {current_value}"  # noqa: E501
     return diff_output
 
 
