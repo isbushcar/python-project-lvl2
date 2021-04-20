34b739a42c42feb245e277757590e3579d9de5e8
berezin <isbushcar@gmail.com>
2021-04-20 19:15:50 +1000

    refactored stylish.py (updated logic of stylish function)

--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -3,44 +3,45 @@
 
 import json
 
+INDENT = ' ' * 4
+COLON = ': '
 
-def stylish(diff_tree, level=0):  # noqa: WPS210
+
+def stylish(diff_tree, level=0):  # noqa: WPS210, WPS231
     """Generate string from diff tree."""
     diff_output = '{\n'
-    indent = '    ' * level
+    indent = INDENT * level
     for key, keys_value in diff_tree.items():
         current_key, status = key if isinstance(key, tuple) else (key, None)
         if status == 'nested':
             inner_diff = stylish(keys_value, level + 1)
-            diff_output += f'{indent}    {current_key}: {inner_diff}\n'
+            diff_output += f'{indent}{INDENT}{current_key}{COLON}{inner_diff}\n'
+        elif status == 'changed':
+            old_value = get_value(keys_value[0], level + 1)
+            current_value = get_value(keys_value[1], level + 1)
+            diff_output += f'{indent}  - {current_key}{COLON}{old_value}\n'
+            diff_output += f'{indent}  + {current_key}{COLON}{current_value}\n'
         else:
-            current_value, old_value = get_value(keys_value, level + 1)
-            lines_template = {
-                'added': f'{indent}  + {current_key}: {current_value}\n',
-                'deleted': f'{indent}  - {current_key}: {current_value}\n',
-                'unchanged': f'{indent}    {current_key}: {current_value}\n',
-                'changed': f'{indent}  - {current_key}: {current_value}\n'  # noqa: E501, WPS221
-                           f'{indent}  + {current_key}: {old_value}\n',  # noqa: E501, WPS318, WPS326
-                None: f'{indent}    {current_key}: {current_value}\n',
-            }
-            diff_output += lines_template[status]
+            current_value = get_value(keys_value, level + 1)
+            if status == 'added':
+                diff_output += f'{indent}  + {current_key}{COLON}{current_value}\n'  # noqa: E501
+            elif status == 'deleted':
+                diff_output += f'{indent}  - {current_key}{COLON}{current_value}\n'  # noqa: E501
+            else:
+                diff_output += f'{indent}{INDENT}{current_key}{COLON}{current_value}\n'  # noqa: E501
     diff_output += indent + '}'
     return diff_output
 
 
 def get_value(keys_value, level=1):
     """Return correct values to add to diff output."""
-    if isinstance(keys_value, tuple):
-        return get_value(keys_value[0], level)[0], (
-            get_value(keys_value[1], level)[0]
-        )
     if not isinstance(keys_value, dict):
-        return json.dumps(keys_value).strip('"'), None
+        return json.dumps(keys_value).strip('"')
     inner_diff = '{\n'
-    indent = '    ' * level
+    indent = INDENT * level
     for inner_key, inner_value in keys_value.items():
         if isinstance(inner_value, dict):
-            inner_value = get_value(inner_value, level + 1)[0]
-        inner_diff += f'{indent}    {inner_key}: {inner_value}\n'
+            inner_value = get_value(inner_value, level + 1)
+        inner_diff += f'{indent}{INDENT}{inner_key}{COLON}{inner_value}\n'
     inner_diff += indent + '}'
-    return inner_diff, None
+    return inner_diff
