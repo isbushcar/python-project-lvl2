da2461895fe1e9fac48fd5b878d7bffbe9282804
berezin <isbushcar@gmail.com>
2021-04-20 19:36:31 +1000

    added one new test to improve test coverage

--- tests/test.py
+++ tests/test.py
@@ -39,6 +39,16 @@ def test_plain_case():
     assert generate_diff(yaml_recursive_file1, yaml_recursive_file2, 'plain') == plain_case_result
 
 
+def test_simple_plain_case():
+    simple_plain_case_result = open(get_path('simple_plain_case_result.txt', 'expected_results')).read()
+    json_file1 = get_path('file1.json', 'fixtures')
+    json_file2 = get_path('file2.json', 'fixtures')
+    assert generate_diff(json_file1, json_file2, 'plain') == simple_plain_case_result
+    yaml_file1 = get_path('file1.yaml', 'fixtures')
+    yaml_file2 = get_path('file2.yaml', 'fixtures')
+    assert generate_diff(yaml_file1, yaml_file2, 'plain') == simple_plain_case_result
+
+
 def test_json_case():
     json_case_result = open(get_path('json_case_result.txt', 'expected_results')).read()
     json_recursive_file1 = get_path('recursive_file1.json', 'fixtures')
